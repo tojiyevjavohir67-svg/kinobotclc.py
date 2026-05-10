672f9903f38842813dabac4770a20cf7335bc3ec
tojiyevjavohir67-svg <tojiyevjavohir67@gmail.com>
2026-05-11 00:19:57 +0500

    Update javohir_test.py

--- javohir_test.py
+++ javohir_test.py
@@ -372,6 +372,5 @@ def run_bot():
 
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 5000))
-    threading.Thread(target=run_bot, daemon=True).start()
-    app.run(host="0.0.0.0", port=PORT, debug=False)
+    import os
+    port = int(os.environ.get("PORT",port=port)
